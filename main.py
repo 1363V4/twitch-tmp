f68251a6284b0425635abb728db0c5573e69e27e
cosmochess <cosmo@chess.com>
2026-03-12 07:45:02 +0000

    dev

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-import json
 import asyncio
-import psutil
 from pathlib import Path
-from uuid import uuid4
 
+import psutil
 from faker import Faker
-
-from watchfiles import run_process
-
-from stario import Context, RichTracer, Stario, Writer, JsonTracer, Relay
-
-from stario import asset, at, data
+from stario import (
+    CompressionConfig,
+    Context,
+    JsonTracer,
+    Relay,
+    RichTracer,
+    Stario,
+    Writer,
+    at,
+    data,
+)
 from stario.html import (
+    H1,
+    H2,
+    H3,
     A,
     B,
     Body,
     Div,
-    H1,
-    H2,
-    H3,
     Head,
     Hr,
     Html,
@@ -28,114 +31,116 @@ from stario.html import (
     Main,
     Meta,
     P,
-    Section,
     Script,
+    Section,
     Source,
     Span,
     Style,
     Tag,
     Title,
     Ul,
-    Video
+    Video,
 )
-
+from watchfiles import run_process
 
 # DATABASE
 
 database = {}
 
 # okay i lied, i need some other stuff as well
-relay = Relay() # for the tick
-fake = Faker() # for the debug
-refresh_rate = 0.03
+relay = Relay()  # for the tick
+fake = Faker()  # for the debug
+refresh_rate = 0.04
 server_stats = ""
 
 # VIEWS
+
 
 def index_init(user_id):
     return Html(
         {"lang": "en"},
         Head(
-            Meta({'charset': "UTF-8"}),
+            Meta({"charset": "UTF-8"}),
             Meta(
                 {"name": "viewport", "content": "width=device-width, initial-scale=1"}
             ),
             Title("cursor party"),
-            Link({
-                'rel': "icon",
-                'href': f"/static/{asset('img/avatar.avif')}"
-            }),
-            Link({
-                'rel': "stylesheet",
-                'href': f"/static/{asset('css/index.css')}"
-            }),
-            Script({
-                'type': "module",
-                # 'src': f"/static/{asset('js/datastar.js')}"
-                'src': "https://cdn.jsdelivr.net/gh/starfederation/datastar@1.0.0-RC.8/bundles/datastar.js"
-            }),
-            Script({
-                'type': "module",
-                'src': f"/static/{asset('js/index.js')}",
-            }),
+            Link({"rel": "icon", "href": "/static/img/avatar.avif')}"}),
+            Link({"rel": "stylesheet", "href": "/static/css/index.css"}),
+            Script(
+                {
+                    "type": "module",
+                    # 'src': f"/static/js/datastar.js')}"
+                    "src": "https://cdn.jsdelivr.net/gh/starfederation/datastar@1.0.0-RC.8/bundles/datastar.js",
+                }
+            ),
+            Script(
+                {
+                    "type": "module",
+                    "src": "/static/js/index.js",
+                }
+            ),
         ),
         Body(
-            {'class': ["gf gc"]},
+            {"class": ["gf gc"]},
             data.init(at.get("/index_cqrs")),
-            ),
-        )
+        ),
+    )
+
 
 def index_view(user_id):
     # if we want to encapsulate
     # WC_Example = Tag("just-flexin", self_closing=True)
     # WC_Example()
     mousetrap = Div(
-        {'id': "mousetrap"},
+        {"id": "mousetrap"},
         *[
-            Div({
-                'class': ["cursor_ai", "gt-s"],
-                'style': f"left: {pos[0]}%; top: {pos[1]}%;"
-            },
-                Img({
-                    'src': f"/static/{asset('img/smol.png')}",
-                }),
+            Div(
+                {
+                    "class": ["cursor_ai", "gt-s"],
+                    "style": f"left: {pos[0]}%; top: {pos[1]}%;",
+                },
+                Img(
+                    {
+                        "src": "/static/img/smol.png",
+                    }
+                ),
                 P(user_id),
             )
             for user_id, pos in database.items()
-        ]
+        ],
     )
     return Body(
-            {'class': ["gf gc"]},
-            data.on('pointermove', "$data = fe_work(evt)"),
-            Main(
-                data.on_interval(at.post("/mouse"), duration=f"{int(refresh_rate*1000)}ms"),
-                {'class': ["gc gt-xxl"]},
-                H1(
-                    "Cursor Party",
-                    data.init("fun()"),
-                    data.ignore_morph(),
-                ),
-                P({'class': "gt-m"}, "Your name is ", Span(user_id)),
-                # P({'class': "gt-s"}, data.json_signals()),
-                P(
-                    {'id': "server"}, 
-                    {'class': "gt-s"}, 
-                    server_stats
-                ),
-                P({
-                    'id': "fps",
-                    'class': ["gt-s"]
-                }, "FPS: ", int(1/refresh_rate)), # kind of a lie
-                mousetrap,
+        {"class": ["gf gc"]},
+        data.on("pointermove", "$data = fe_work(evt)"),
+        Main(
+            data.on_interval(
+                at.post("/mouse"), duration=f"{int(refresh_rate * 1000)}ms"
             ),
-        )
+            {"class": ["gc gt-xxl"]},
+            H1(
+                "Cursor Party",
+                data.init("fun()"),
+                data.ignore_morph(),
+            ),
+            P({"class": "gt-m"}, "Your name is ", Span(user_id)),
+            # P({'class': "gt-s"}, data.json_signals()),
+            P({"id": "server"}, {"class": "gt-s"}, server_stats),
+            P(
+                {"id": "fps", "class": ["gt-s"]}, "FPS: ", int(1 / refresh_rate)
+            ),  # kind of a lie
+            mousetrap,
+        ),
+    )
+
 
 # HANDLERS
+
 
 async def index(c: Context, w: Writer):
     # user_id = uuid4()
     # bad boy, no user_id until you're grown up
-    user_id = c.req.cookies.get('user_id')
+    user_id = c.req.cookies.get("user_id")
     if not user_id:
         user_id = fake.name()
         w.cookie("user_id", user_id, httponly=True, secure=True)
@@ -144,10 +149,11 @@ async def index(c: Context, w: Writer):
     database[user_id] = [0, 0]
     w.html(index_init(user_id))
 
+
 async def index_cqrs(c: Context, w: Writer):
-    user_id = c.req.cookies.get('user_id')
+    user_id = c.req.cookies.get("user_id")
     if user_id and user_id not in database:
-        database[user_id] = [0, 0]   
+        database[user_id] = [0, 0]
         # okay that was it
     # here, w.alive() ends when the SSE connection disconnects
     async for _ in w.alive(relay.subscribe("refresh")):
@@ -158,23 +164,27 @@ async def index_cqrs(c: Context, w: Writer):
     if user_id and user_id in database:
         del database[user_id]
 
+
 async def mouse(c: Context, w: Writer):
     # careful, data keeps coming when window inactive
     signals = await c.signals()
-    data = signals.get('data')
+    data = signals.get("data")
     if data:
-        user_id = c.req.cookies.get('user_id')
+        user_id = c.req.cookies.get("user_id")
         if user_id and user_id in database:
             dx, dy = data
             database[user_id] = [dx, dy]
     w.empty()
 
+
 # LOOPS
+
 
 async def refresh(refresh_rate=refresh_rate):
     while True:
         relay.publish("refresh", "")
         await asyncio.sleep(refresh_rate)
+
 
 async def wassup_psutil():
     global server_stats
@@ -192,16 +202,24 @@ async def wassup_psutil():
         )
         await asyncio.sleep(1)
 
+
 # APP
 
+
 async def main():
-    database = {} # poor man's reset
+    database.clear()
     asyncio.create_task(refresh())
     asyncio.create_task(wassup_psutil())
     # with RichTracer() as tracer:
     with JsonTracer() as tracer:
-        app = Stario(tracer)
-
+        app = Stario(
+            tracer,
+            # compression=CompressionConfig(
+            #     # min_size=1024,     # Minimum bytes to compress... what?
+            #     # zstd_level=4,  # 1-22 (3 default)
+            #     brotli_level=4
+            # ),
+        )
         app.get("/", index)
         app.get("/index_cqrs", index_cqrs)
         app.post("/mouse", mouse)
@@ -210,12 +228,11 @@ async def main():
         await app.serve(unix_socket="/run/legovh/tmp.sock")
         # await app.serve()
 
+
 def serve():
     asyncio.run(main())
 
+
 if __name__ == "__main__":
     # serve()
-    run_process(
-        Path(__file__).parent,
-        target=serve
-    )
+    run_process(Path(__file__).parent, target=serve)
